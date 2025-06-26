6147653a1daad0433eb14e5d65d577891cf4eb88
RIFANSYAH <syahr9610@gmail.com>
2025-06-26 17:49:27 +0700

    fitur gambar dan berita

--- resources/views/admin/galeri/foto/index.blade.php
+++ resources/views/admin/galeri/foto/index.blade.php
@@ -2,53 +2,44 @@
 
 @section('content')
 <div class="container-fluid">
-    <h2 class="mb-4">Kelola Berita</h2>
+    <h2 class="mb-4">Kelola Gambar</h2>
 
-    <a href="#" class="btn btn-primary mb-3">Tambah Berita</a>
+    <a href="{{ route('galeri.create') }}" class="btn btn-primary mb-3">Tambah Gambar</a>
 
     <div class="card shadow-sm">
         <div class="card-body">
             <div class="table-responsive">
-                <table id="beritaTable" class="table table-bordered table-hover w-100">
+                <table id="galeriTable" class="table table-bordered table-hover w-100">
                     <thead class="thead-dark">
                         <tr>
                             <th style="width: 5%;">No</th>
-                            <th style="width: 15%;">Foto</th>
-                            <th style="width: 20%;">Judul</th>
-                            <th style="width: 15%;">Tanggal</th>
-                            <th style="width: 30%;">Isi Penulisan</th>
-                            <th style="width: 15%;">Aksi</th>
+                            <th style="width: 20%;">Gambar</th>
+                            <th style="width: 50%;">judul</th>
+                            <th style="width: 25%;">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <!-- Data dummy sementara -->
-                        <tr>
-                            <td>1</td>
-                            <td>
-                                <img src="https://via.placeholder.com/100" alt="Foto" class="img-thumbnail">
-                            </td>
-                            <td>Judul Berita Pertama</td>
-                            <td>2025-06-25</td>
-                            <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</td>
-                            <td>
-                                <a href="#" class="btn btn-sm btn-warning mb-1">Edit</a>
-                                <button onclick="return confirm('Yakin ingin menghapus?')" class="btn btn-sm btn-danger">Hapus</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>
-                                <img src="https://via.placeholder.com/100" alt="Foto" class="img-thumbnail">
-                            </td>
-                            <td>Judul Berita Kedua</td>
-                            <td>2025-06-24</td>
-                            <td>Berita ini berisi informasi terbaru mengenai kegiatan desa...</td>
-                            <td>
-                                <a href="#" class="btn btn-sm btn-warning mb-1">Edit</a>
-                                <button onclick="return confirm('Yakin ingin menghapus?')" class="btn btn-sm btn-danger">Hapus</button>
-                            </td>
-                        </tr>
-                        <!-- Tambah data dummy lainnya sesuai kebutuhan -->
+                        @forelse ($galeri as $item)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>
+                                    <img src="{{ asset('storage/' . $item->file) }}" alt="Gambar" class="img-thumbnail" style="max-width: 100px;">
+                                </td>
+                                <td>{{ $item->deskripsi }}</td>
+                                <td>
+                                    <a href="{{ route('galeri.edit', $item->id) }}" class="btn btn-sm btn-warning mb-1">Edit</a>
+                                    <form action="{{ route('galeri.destroy', $item->id) }}" method="POST" class="d-inline">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button onclick="return confirm('Yakin ingin menghapus?')" class="btn btn-sm btn-danger">Hapus</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="4" class="text-center">Belum ada gambar.</td>
+                            </tr>
+                        @endforelse
                     </tbody>
                 </table>
             </div>
@@ -56,7 +47,6 @@
     </div>
 </div>
 @endsection
-
 @section('scripts')
 <!-- DataTables CSS & JS -->
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
@@ -65,23 +55,21 @@
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
 
 <style>
+    /* Tambahan untuk scrollbar */
     div.dataTables_wrapper div.dataTables_scrollBody {
         max-height: 300px !important;
     }
 
+    /* Tambahan untuk tampilan rapih */
     table.dataTable td img {
         display: block;
         margin: 0 auto;
-    }
-
-    td {
-        vertical-align: middle !important;
     }
 </style>
 
 <script>
     $(document).ready(function () {
-        $('#beritaTable').DataTable({
+        $('#galeriTable').DataTable({
             scrollY: '300px',
             scrollX: true,
             scrollCollapse: true,
@@ -91,13 +79,12 @@
             pageLength: 5,
             columnDefs: [
                 { width: '5%', targets: 0 },
-                { width: '15%', targets: 1 },
-                { width: '20%', targets: 2 },
-                { width: '15%', targets: 3 },
-                { width: '30%', targets: 4 },
-                { width: '15%', targets: 5 }
+                { width: '20%', targets: 1 },
+                { width: '50%', targets: 2 },
+                { width: '25%', targets: 3 },
             ]
         });
     });
 </script>
+
 @endsection
